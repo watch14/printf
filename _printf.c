13bfd0dd6ec84329f98e57e155690444224e6651
watch14 <watchman1414@gmail.com>
2023-08-20 14:32:02 +0000

    fast_push

--- _printf.c
+++ _printf.c
@@ -1,41 +1,26 @@
 #include "main.h"
+
 /**
  * _printf - Prints formatted output to the standard output stream.
  * @format: The format string containing zero or more directives.
  *
  * Return: The number of characters printed.
  */
+
 int _printf(const char *format, ...)
 {
 	va_list args;
-	int i, nbch = 0;
+	unsigned int i, nbch = 0;
 
 	va_start(args, format);
-	for (i = 0 ; format[i] != '\0' ; i++)
+	for (i = 0; format[i] != '\0'; i++)
 	{
 		if (format[i] == '%')
 		{
 			i++;
-			if (format[i] == 'c')
-			{
-				char c = (char)va_arg(args, int);
+			char *s = va_arg(args, char *);
 
-				write(1, &c, 1);
-				nbch++;
-			} else if (format[i] == 's')
-			{
-				char *s = va_arg(args, char *);
-
-				write(1, s, str_len(s));
-				nbch += str_len(s);
-			}
-			else if (format[i] == '%')
-			{
-				char s = '%';
-
-				write(1, &s, 1);
-				nbch++;
-			}
+			nbch = check(format[i], s, nbch);
 		}
 		else
 		{
--- functions.c
+++ functions.c
@@ -1,4 +1,34 @@
 #incluide "main.h"
+/**
+ * check - Handles different format specifiers.
+ * @f: The format specifier.
+ * @s: The string argument.
+ * @nbch: The current number of characters printed.
+ *
+ * Return: The updated number of characters printed.
+ */
+
+int check(char f, char *s, int nbch)
+{
+	if (f == 'c')
+	{
+		write(1, s, 1);
+		nbch++;
+	}
+	else if (f == 's')
+	{
+		write(1, s, str_len(s));
+		nbch += str_len(s);
+	}
+	else if (f == '%')
+	{
+		char c = '%';
+
+		write(1, &c, 1);
+		nbch++;
+	}
+	return (nbch);
+}
 
 /**
  * str_len - Calculates the length of a string.
