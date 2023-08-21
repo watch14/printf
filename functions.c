697764b72d284c35734136e325b3b7c88677f506
watch14 <watchman1414@gmail.com>
2023-08-21 08:47:18 +0000

    fast_push

--- functions.c
+++ functions.c
@@ -6,36 +6,34 @@
  * @args: The variable argument list.
  * @count: Pointer to the count of characters printed.
  */
-
 void print_specifier(char specifier, va_list args, int *count)
 {
-    switch (specifier)
-    {
-        case 'c':
-            putchar(va_arg(args, int));
-            (*count)++;
-            break;
-        case 's':
-            (*count) += printf("%s", va_arg(args, char *));
-            break;
-        case '%':
-            putchar('%');
-            (*count)++;
-            break;
-        case 'i':
-        case 'd':
-            (*count) += printf("%d", va_arg(args, int));
-            break;
-        default:
-            putchar('%');
-            putchar(specifier);
-            (*count) += 2;
-            break;
-    }
+	switch (specifier)
+	{
+		case 'c':
+			putchar(va_arg(args, int));
+			(*count)++;
+			break;
+		case 's':
+			(*count) += printf("%s", va_arg(args, char *));
+			break;
+		case '%':
+			putchar('%');
+			(*count)++;
+			break;
+		case 'i':
+		case 'd':
+			(*count) += printf("%d", va_arg(args, int));
+			break;
+		default:
+			putchar('%');
+			putchar(specifier);
+			(*count) += 2;
+			break;
+	}
 }
-
-/** 
- * str_len - Calculates the length of a string.
+/**
+ * str_len - Calculate the length of a string.
  * @str: The input string.
  *
  * Return: The length of the string.
