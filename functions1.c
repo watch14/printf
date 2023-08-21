f148e266db3f7ab4c94def8b912a4cb8a5185132
watch14 <watchman1414@gmail.com>
2023-08-21 17:49:23 +0000

    fast_push

--- functions1.c
+++ functions1.c
@@ -71,53 +71,6 @@ int check_d(va_list args, int count)
 	return (count);
 }
 
-/**
- * check_b - Prints an unsigned integer in binary format.
- * @args: The variable argument list.
- * @count: The current count of characters printed.
- * 
- * Return: The updated count of characters printed.
- */
-int check_b(va_list args, int count)
-{
-	unsigned int n = va_arg(args, unsigned int);
-
-	if (n == 0)
-	{
-		char c = '0';
-
-		write(1, &c, 1);
-		count++;
-	}
-	else if (n == 1)
-	{
-		char c = '1';
-
-		write(1, &c, 1);
-		count++;
-	}
-	else
-	{
-		char binary[32];
-		int i = 0;
-
-		while (n > 0)
-		{
-			binary[i] = (n % 2) + '0';
-			n /= 2;
-			i++;
-		}
-
-		while (i > 0)
-		{
-			i--;
-			write(1, &binary[i], 1);
-			count++;
-		}
-	}
-
-	return (count);
-}
 
 /**
  * check - Handles the printing of conversion specifiers.
