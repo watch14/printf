a4447812d3ded0776104b317c0a8bdeb26de42dc
watch14 <watchman1414@gmail.com>
2023-08-21 08:25:02 +0000

    fast_push

--- functions.c
+++ functions.c
@@ -1,4 +1,4 @@
-#incluide "main.h"
+#include "main.h"
 
 /**
  * print_specifier - Handle the printing of conversion specifiers.
--- main.h
+++ main.h
@@ -1,15 +1,20 @@
 #ifndef MAIN_H
 #define MAIN_H
-
 #include <stdio.h>
 #include <string.h>
 #include <stdarg.h>
 #include <unistd.h>
 
+
 #define BUFF_SIZE 1024
+
 
 int _printf(const char *format, ...);
 void print_specifier(char specifier, va_list args, int count);
 int str_len(char *str);
+
+
+
+
 
 #endif
