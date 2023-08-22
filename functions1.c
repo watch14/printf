341a18aacfffbdf381bf95c7a2771a27a8496248
watch14 <watchman1414@gmail.com>
2023-08-22 11:23:18 +0000

    fast_push

--- functions1.c
+++ functions1.c
@@ -93,8 +93,6 @@ int check(char specifier, va_list args, int count)
 			count = check_s(args, count);
 			break;
 		case '%':
-			write(1, &specifier, 1);
-			count++;
 			break;
 		case 'i':
 		case 'd':
